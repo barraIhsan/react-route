bcd6490b4308b1cc1656c49c0047ac14ae292ff4
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-05-28 13:57:50 +0700

    feat: add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Navbar";
 import background from "!/img/bg.jpg";
+import Hero from "./Hero";
 
 export default function Home() {
   return (
     <body style={{ backgroundImage: `url(${background.src})` }}>
       <Navbar />
+      <Hero />
     </body>
   );
 }
